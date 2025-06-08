3f60d7ff10ebe4f7467408def5141e646acf5d50
Roxy-Magic <154532202+Roxy-Magic@users.noreply.github.com>
2025-06-08 07:03:06 +0000

    1 Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/07/hello-world/","2025/06/08/test/"];function toRandomPost(){
+var posts=["2025/06/08/hello-world/","2025/06/08/test/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
