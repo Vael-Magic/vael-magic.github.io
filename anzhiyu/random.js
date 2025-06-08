85e808cedf430775fb835d8db872b20affe8f061
Roxy-Magic <154532202+Roxy-Magic@users.noreply.github.com>
2025-06-08 07:33:04 +0000

    测试提交 Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/08/hello-world/","2025/06/08/post/"];function toRandomPost(){
+var posts=["2025/06/08/post/","2025/06/08/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
